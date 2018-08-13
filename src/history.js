f10a3b18ae6faf90c96a1fde664d695d06b31711
Vadim Bubnov <vadim@eventbrite.com>
2018-08-13 11:31:33 -0300

    Updates

--- src/history.js
+++
@@ -1,9 +0,0 @@
-import {browserHistory, createMemoryHistory} from 'react-router';
-
-const getHistory = () => {
-    let hasBrowserHistory = typeof(browserHistory) !== 'undefined';
-
-    return hasBrowserHistory ? browserHistory : createMemoryHistory();
-};
-
-export default getHistory;
