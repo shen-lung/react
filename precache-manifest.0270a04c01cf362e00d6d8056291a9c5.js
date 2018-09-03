24e69b9e7253d6856ce976aad2a67d40eaf7ff4e
Vadim Bubnov <vadim@eventbrite.com>
2018-09-03 11:50:47 -0300

    Updates

---
+++ precache-manifest.0270a04c01cf362e00d6d8056291a9c5.js
@@ -0,0 +1,13 @@
+self.__precacheManifest = [
+  {
+    "url": "c33873b0cdaf3192972c7317a5ccfba2.svg"
+  },
+  {
+    "revision": "c697f6586ed04d661d27",
+    "url": "bundle.js"
+  },
+  {
+    "revision": "02b6e6c10dbd29d3eb978d9b1252c3d5",
+    "url": "./index.html"
+  }
+];
